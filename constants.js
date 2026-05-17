52462d6943e2453ffd85a98a6b3521143ac73b20
Ayub <sameerbaig690@gamil.com>
2026-05-17 23:45:34 +0530

    Add contants

---
+++ constants.js
@@ -0,0 +1,15 @@
+const PLATFORMS = ["instagram", "youtube", "tiktok"];
+const TONES = [
+    "motivational",
+    "funny",
+    "educational",
+    "cinematic",
+    "professional"
+];
+const DURATIONS = [15, 30];
+
+export {
+    PLATFORMS,
+    TONES,
+    DURATIONS
+}
